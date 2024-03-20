97801140e47b4d638dd88e9a99904a8e9ea036b7
Mamaly1000 <105161078+Mamaly1000@users.noreply.github.com>
2024-03-20 21:16:47 +0330

    Update reservation filter modal with form

--- src/components/modals/FilterReservationModal.tsx
+++ src/components/modals/FilterReservationModal.tsx
@@ -1,13 +1,26 @@
 "use client";
 import { useReservationFilterModal } from "@/hooks/useReservationFilterModal";
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import Modal from "./Modal";
 import Heading from "../form/Heading";
 import { useReservationTable } from "@/hooks/useReservationTable";
-import PriceRange from "../search-page/PriceRange";
-import CustomSelect from "../inputs/CustomSelect";
-
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+const reservatoinFilterFormType = z.object({
+  min: z.number().optional(),
+  max: z.number().optional(),
+  userId: z.string().optional(),
+  listingId: z.string().optional(),
+  type: z.enum(["COMPLETED", "PENDING", "ALL"]).default("ALL"),
+});
+enum STEPS {
+  AMOUNT,
+  INFO,
+  TYPE,
+}
 const FilterReservationModal = () => {
+  const [step, setStep] = useState(STEPS.AMOUNT);
   const { isOpen, onClose } = useReservationFilterModal();
   const {
     SelectedFilters,
@@ -16,23 +29,55 @@ const FilterReservationModal = () => {
     setQuery,
     onResetQuery,
   } = useReservationTable();
-  const [price, setPrice] = useState({ min: 0, max: 1000 });
+  const form = useForm({
+    resolver: zodResolver(reservatoinFilterFormType),
+    values: {
+      min: undefined,
+      max: undefined,
+      userId: undefined,
+      listingId: undefined,
+      type: "all",
+    },
+  });
+  const body = useMemo(() => {
+    if (step === STEPS.AMOUNT) {
+      return (
+        <div className="min-w-full max-w-full min-h-fit flex flex-col items-start justify-start gap-8 bg-white dark:bg-neutral-900">
+          <Heading
+            title="what total amount do you want to filter?"
+            subtitle="you can leave it empty for no limit"
+          />
+        </div>
+      );
+    }
+    if (step === STEPS.INFO) {
+      return (
+        <div className="min-w-full max-w-full min-h-fit flex flex-col items-start justify-start gap-8 bg-white dark:bg-neutral-900">
+          <Heading
+            title="additional information to filter(listingId,userId)"
+            subtitle="you can selecy specific user or listing for filtering your reservations."
+          />
+        </div>
+      );
+    }
+    if (step === STEPS.TYPE) {
+      return (
+        <div className="min-w-full max-w-full min-h-fit flex flex-col items-start justify-start gap-8 bg-white dark:bg-neutral-900">
+          <Heading
+            title="filter by reservation type:"
+            subtitle="you can filter by all types of reservations here."
+          />
+        </div>
+      );
+    }
+  }, [form, step]);
   return (
     <Modal
       isOpen={isOpen}
       onChange={(val) => {
         if (!val) onClose();
       }}
-      body={
-        <div className="min-w-full max-w-full min-h-fit flex flex-col items-start justify-start gap-8 bg-white dark:bg-neutral-900">
-          <Heading
-            title="filter your reservations data"
-            subtitle="you can filter a range of data for reservation."
-          />
-          <PriceRange handlePriceRangeChange={setPrice} />
-          <CustomSelect onChange={(val) => {}} options={[]} value={undefined} />
-        </div>
-      }
+      body={body}
       header={{
         title: "Update reservation",
         close: () => {
@@ -44,7 +89,6 @@ const FilterReservationModal = () => {
           label: "update now",
           onClick: () => {
             // onClose();
-            console.log({ price });
           },
         },
       }}
